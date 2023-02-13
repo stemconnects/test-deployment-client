e607f509097b680de9ecc713851256163aecf741
G-McCarten <76708068+G-McCarten@users.noreply.github.com>
2023-02-13 17:34:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import Home from "./pages/Home";
+import CreatePosts from './pages/CreatePosts';
+import React from 'react'
+import Post from './pages/Post';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
+import {AuthContext} from './helpers/AuthContext'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
+  const [authState, setAuthState] = useState({username: "", id: 0, status: false});
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/auth/auth', {headers: {
+      accessToken: localStorage.getItem('accessToken'),
+    }
+  })
+  .then((response) => {
+      if(response.data.error) {
+        setAuthState({...authState, status: false})
+      } else{
+        setAuthState({username: response.data.username, id: response.data.id, status: true})
+      }
+    })
+  }, [])
+  
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setAuthState({username: "", id: 0, status: false})
+  }
+
+
+  return ( 
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <AuthContext.Provider value={{authState, setAuthState}}>
+      <Router>
+        <div className="navbar">
+          <Link to="/createpost"> Create A Post</Link>
+          <Link to="/"> Home Page</Link>
+          {!authState.status ? (
+            <>
+            <Link to="/login"> Login</Link>
+            <Link to="/registration"> Registration</Link>
+            </>
+          ) : (
+            <button onClick={logout}>Logout</button>
+          )}
+
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createpost" element={<CreatePosts />} />
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="post/:id" element={<Post />} />
+        </Routes>
+      </Router> 
+      </AuthContext.Provider>
+    </div> 
   );
 }
 
---
+++ src/pages/CreatePosts.js
@@ -0,0 +1,48 @@
+import React from 'react'
+//Allows to create forms with out using tradition html tags, they already create components for you
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
+import axios from "axios";
+import { useNavigate } from 'react-router-dom'
+
+function CreatePosts() {
+    const initialValues = {
+        title: "",
+        postText: "",
+        username: ""
+    };
+
+    const validationSchema = Yup.object().shape({
+        title: Yup.string().required("You must input a title"),
+        postText: Yup.string().required(),
+        username: Yup.string().min(3).max(15).required(),
+    })
+    
+    const onSubmit = (data) =>{
+        axios.post("http://localhost:3001/posts", data).then( (response) => {
+            navigate("/");
+    });
+    }
+
+    let navigate = useNavigate();
+    return (
+        <div className='createPostPage'>
+            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                <Form className='formContainer'>
+                    <label>Title:</label>
+                    <ErrorMessage name="title" component="span" />
+                    <Field id="inputCreatePost" name="title" placeholder="Title..."></Field>
+                    <label>Post:</label>
+                    <ErrorMessage name="postText" component="span" />
+                    <Field id="inputCreatePost" name="postText" placeholder="Post..."></Field>
+                    <label>Username:</label>
+                    <ErrorMessage name="username" component="span" />
+                    <Field id="inputCreatePost" name="username" placeholder="John..."></Field>
+                    <button type="submit"> Create Post</button>
+                </Form>
+            </Formik>
+        </div>
+  )
+}
+
+export default CreatePosts
